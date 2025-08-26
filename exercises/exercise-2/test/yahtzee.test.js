9c86727e401de9b5e8e44b23ebf8cda852f3efc3
moab01567 <moh080203@gmail.com>
2025-08-26 18:15:48 +0200

    added node tope level

--- exercises/exercise-2/test/yahtzee.test.js
+++ exercises/exercise-2/test/yahtzee.test.js
@@ -1,17 +1,17 @@
-import { expect, test } from 'vitest'
+import { expect, test } from "vitest";
 
 function yatzeeScore(chance, numbers) {
-    var sum = 0;
-    numbers.forEach(number => sum += number)
-    return sum;
+  var sum = 0;
+  numbers.forEach((number) => (sum += number));
+  return sum;
 }
 
-function yatzeeOnes(numbers){
-    var counter= 0
-    numbers.forEach(number => number === 1? counter++ : 0)
-    return counter
+function yatzeeOnes(numbers) {
+  var counter = 0;
+  numbers.forEach((number) => (number === 1 ? counter++ : 0));
+  return counter;
 }
 
 test("scoring Chance add all dice", () => {
-    expect(yatzeeScore("Chance", [1, 2, 3, 4, 5])).toBe(1+2+3+4+5);
+  expect(yatzeeScore("Chance", [1, 2, 3, 4, 5])).toBe(1 + 2 + 3 + 4 + 5);
 });
