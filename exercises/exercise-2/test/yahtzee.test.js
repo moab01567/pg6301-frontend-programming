5003eda79b1770702c391a340c07673971941454
moab01567 <moh080203@gmail.com>
2025-08-26 17:31:24 +0200

    test passed

--- exercises/exercise-2/test/yahtzee.test.js
+++ exercises/exercise-2/test/yahtzee.test.js
@@ -1,4 +1,10 @@
+import { expect, test } from 'vitest'
 
+function yatzeeScore(chance, numbers) {
+    var sum = 0;
+    numbers.forEach(number => sum += number)
+    return sum;
+}
 
 test("scoring Chance add all dice", () => {
     expect(yatzeeScore("Chance", [1, 2, 3, 4, 5])).toBe(1+2+3+4+5);
