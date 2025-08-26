b3b3edc2eba7b92390d87731c25bcb195f258440
moab01567 <moh080203@gmail.com>
2025-08-26 17:57:18 +0200

    not added test yet

--- exercises/exercise-2/test/yahtzee.test.js
+++ exercises/exercise-2/test/yahtzee.test.js
@@ -6,6 +6,12 @@ function yatzeeScore(chance, numbers) {
     return sum;
 }
 
+function yatzeeOnes(numbers){
+    var counter= 0
+    numbers.forEach(number => number === 1? counter++ : 0)
+    return counter
+}
+
 test("scoring Chance add all dice", () => {
     expect(yatzeeScore("Chance", [1, 2, 3, 4, 5])).toBe(1+2+3+4+5);
 });
